40959fecd5e574be891cf54ba6a200e93e143ef0
yasushi sakita <sakita@tasdg.co.jp>
2014-05-29 21:06:05 +0900

    Slimフレームワークにて、Twigテンプレートエンジンと連携

--- index.php
+++ index.php
@@ -1,130 +1,18 @@
 <?php
 // load required files
 require 'Slim/Slim.php';
-require 'RedBean/rb.php';
-
-// register Slim auto-loader
 \Slim\Slim::registerAutoloader();
+require 'Twig/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
 
-// set up database connection
-R::setup('mysql:host=localhost;dbname=slim','root','root');
-R::freeze(true);
-// initialize app
-$app = new \Slim\Slim();
+$app = new \Slim\Slim(array(
+  'debug' => true,
+  'view' => new \Slim\Extras\Views\Twig(),
+));
 
-class ResourceNotFoundException extends Exception {}
-
-// handle GET requests for /articles/:id
-$app->get('/articles/:id', function ($id) use ($app) {
-  try {
-    // query database for single article
-    $article = R::findOne('articles', 'id=?', array($id));
-
-    if ($article) {
-      // if found, return JSON response
-      $app->response()->header('Content-Type', 'application/json');
-      echo json_encode(R::exportAll($article));
-    } else {
-      // else throw exception
-      throw new ResourceNotFoundException();
-    }
-  } catch (ResourceNotFoundException $e) {
-    // return 404 server error
-    $app->response()->status(404);
-  } catch (Exception $e) {
-    $app->response()->status(400);
-    $app->response()->header('X-Status-Reason', $e->getMessage());
-  }
+$app->get('/', function() use ($app) {
+  $app->render('index.html.twig', array('name' => 'batman'));
 });
 
-// handle GET requests for /articles
-$app->get('/articles', function () use ($app) {
-  // query database for all articles
-  $articles = R::find('articles');
-
-  // send response header for JSON content type
-  $app->response()->header('Content-Type', 'application/json');
-
-  // return JSON-encoded response body with query results
-  echo json_encode(R::exportAll($articles));
-});
-
-// handle POST requests to /articles
-$app->post('/articles', function () use ($app) {
-  try {
-    // get and decode JSON request body
-    $request = $app->request();
-    $body = $request->getBody();
-    $input = json_decode($body);
-
-    // store article record
-    $article = R::dispense('articles');
-    $article->title = (string)$input->title;
-    $article->url = (string)$input->url;
-    $article->date = (string)$input->date;
-    $id = R::store($article);
-
-    // return JSON-encoded response body
-    $app->response()->header('Content-Type', 'application/json');
-    echo json_encode(R::exportAll($article));
-  } catch (Exception $e) {
-    $app->response()->status(400);
-    $app->response()->header('X-Status-Reason', $e->getMessage());
-  }
- });
-
-$app->put('/articles/:id', function ($id) use ($app) {
-  try {
-    // get and decode JSON request body
-    $request = $app->request();
-    $body = $request->getBody();
-    $input = json_decode($body);
-
-    // query database for single article
-    $article = R::findOne('articles', 'id=?', array($id));
-
-    // store modified article
-    // return JSON-encoded response body
-    if ($article) {
-      $article->title = (string)$input->title;
-      $article->url = (string)$input->url;
-      $article->date = (string)$input->date;
-      R::store($article);
-      $app->response()->header('Content-Type', 'application/json');
-      echo json_encode(R::exportAll($article));
-    } else {
-      throw new ResourceNotFoundException();
-    }
-  } catch (ResourceNotFoundException $e) {
-    $app->response()->status(404);
-  } catch (Exception $e) {
-    $app->response()->status(400);
-    $app->response()->header('X-Status-Reason', $e->getMessage());
-  }
-});
-
-// handle DELETE requests to /articles/:id
-$app->delete('/articles/:id', function ($id) use ($app) {
-  try {
-    // query database for article
-    $request = $app->request();
-    $article = R::findOne('articles', 'id=?', array($id));
-
-    // delete article
-    if ($article) {
-      R::trash($article);
-      $app->response()->status(204);
-    } else {
-      throw new ResourceNotFoundException();
-    }
-  } catch (ResourceNotFoundException $e) {
-    $app->response()->status(404);
-  } catch (Exception $e) {
-    $app->response()->status(400);
-    $app->response()->header('X-Status-Reason', $e->getMessage());
-  }
-});
-
-
-// run
+/* Run the application */
 $app->run();
