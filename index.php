b2f47b248183ee3f72ba095b3a68e5e2399473cd
yasushi sakita <sakita@tasdg.co.jp>
2014-05-30 16:33:50 +0900

    ごみファイルの削除と一旦コミット

--- index.php
+++ index.php
@@ -1,16 +1,66 @@
 <?php
-// load required files
+/* Slimフレームワーク */
 require 'Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
+/* Twig連携 */
 require 'Twig/lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
+/* O/Rマッパー */
+require 'RedBean/rb.php';
+
+// set up database connection
+R::setup('mysql:host=localhost;dbname=slim', 'root', 'root');
+R::freeze(true);
+
+class ResourceNotFoundException extends Exception
+{
+}
+
+/**
+ *
+ * @param \Slim\Route $route
+ */
+function authenticate(\Slim\Route $route)
+{
+  $app = \Slim\Slim::getInstance();
+  $uid = $app->getEncryptedCookie('uid');
+  $key = $app->getEncryptedCookie('key');
+  if (validateUserKey($uid, $key) === false) {
+    $app->halt(401);
+  }
+}
+
+/**
+ *
+ * @param $uid
+ * @param $key
+ * @return bool
+ */
+function validateUserKey($uid, $key)
+{
+  if ($uid === 'batman' && $key === 'dark-knight') {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 $app = new \Slim\Slim(array(
   'debug' => true,
   'view' => new \Slim\Extras\Views\Twig(),
 ));
 
-$app->get('/', function() use ($app) {
+$app->get('/cookie', function () use ($app) {
+  try {
+    $app->setEncryptedCookie('uid', 'batman', '10 minutes');
+    $app->setEncryptedCookie('key', 'dark-knight', '10 minutes');
+  } catch (Exception $e) {
+    $app->response()->status(400);
+    $app->response()->header('X-Status-Reason', $e->getMessage());
+  }
+});
+
+$app->get('/', function () use ($app) {
   $app->render('index.html.twig', array('name' => 'batman'));
 });
 
